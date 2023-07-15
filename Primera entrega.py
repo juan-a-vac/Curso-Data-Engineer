333ed2c63b40a041be9dabca9c4951af7f4a5bc7
Juan Vaca <prueba@gmail.com>
2023-07-15 20:02:37 -0300

    cambio a api de frankfurter

--- Primera entrega.py
+++ Primera entrega.py
@@ -1,8 +1,11 @@
+#info tomada de https://www.frankfurter.app/
 import requests
 import json
 
 #requests
-url = 'https://fakestoreapi.com/products/1'
+url = 'https://www.frankfurter.app/currencies'
 r = requests.get(url)
 j = r.json()
 print(j)
+#print(j.keys())
+#dict_keys(['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'IDR', 'ILS', 'INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR'])
