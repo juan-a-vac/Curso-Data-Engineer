8c4afa35e6a5df125bd46db2cb5a14d208bb828f
Juan Vaca <prueba@gmail.com>
2023-08-24 19:36:44 -0300

    agregado masivo de datos

--- Segunda entrega/Segunda entrega.py
+++ Segunda entrega/Segunda entrega.py
@@ -1,4 +1,5 @@
 #info tomada de https://www.frankfurter.app/
+
 import requests
 import json
 import pandas as pd
@@ -15,6 +16,28 @@ port_id= 5439
 engine= create_engine("postgresql://juan_a_vaca_11_coderhouse:laJ7b0x4v5@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database")
 
 #engine= conn = create_engine('postgresql://username:password@yoururl.com:5439/yourdatabase')
+#cargamos datos desde el primero de enero de 2020
+
+url1 = 'https://www.frankfurter.app/2021-02-02..2023-08-15'
+r1 = requests.get(url1)
+j1 = r1.json()
+j2 = pd.read_json(j1)
+
+date = pd.DataFrame(j1['rates'])
+date = date.T                                       #   traspongo la matriz
+date.reset_index(inplace = True)
+date.rename(columns={date.columns[0]:'Fecha'},inplace=True)
+
+print(date)
+# hasta aca obtenemos el DF principal historico
+# Necesitamos una PK compuesta
+date['PK_Compuesta'] = date.index.astype(str) + '_' + date[date.columns[0]].astype(str)
+date['Fecha'] = pd.to_datetime(date['Fecha'])
+#agregamos los datos a la tabla de redshift
+date.to_sql('comparacion_divisas', engine, if_exists='replace', index=False)#reemplazamos ya que esta linea solo se ejecuta una vez
+
+#agregado final de datos masivos
+'''
 #requests
 url = 'https://www.frankfurter.app/latest'
 r = requests.get(url)
@@ -40,5 +63,4 @@ df_final['date'] = pd.to_datetime(df_final['date'])
 #agregamos los datos a la tabla de redshift
 df_final.to_sql('comparacion_divisas', engine, if_exists='append', index=False)
 df_a = pd.read_sql_table('comparacion_divisas',engine)
-
-
+'''
